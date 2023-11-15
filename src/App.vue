40696f108846a4666b583b1ef048196f36eb3305
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-11-15 11:42:45 +0530

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SubmitCustomerData />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SubmitCustomerData from './components/SubmitCustomerData.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SubmitCustomerData
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
