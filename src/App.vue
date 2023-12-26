f6940550ac872bbd6f3dd55e919cd81be3d6a9b6
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-12-26 21:10:47 +0530

    added plugin and used qr generator

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <SubmitCustomerData />
+  <div>
+    <QRGenerator />
+  </div>
 </template>
 
 <script>
-import SubmitCustomerData from './components/SubmitCustomerData.vue';
+import { QRGenerator } from "ui-lib-utils";
+import "ui-lib-utils/dist/ui-lib-utils.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    SubmitCustomerData
-  }
-}
+    QRGenerator,
+  },
+};
 </script>
 
 <style>
+.QRGenerator {
+  margin: 50px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+}
 </style>
